d816f45013746c481baeb66acd4bd7a9362494f8
duncanxia <duncanxia97@gmail.com>
2025-06-16 10:06:43 +0800

    修复bug

--- src/Abstracts/AbstractFormRequestParam.php
+++ src/Abstracts/AbstractFormRequestParam.php
@@ -7,9 +7,55 @@
 namespace Fatbit\FormRequestParam\Abstracts;
 
 use Fatbit\FormRequestParam\Traits\FormRequestParam;
+use Hyperf\Context\ApplicationContext;
+use Hyperf\Context\Context;
+use Hyperf\Engine\Contract\Http\V2\RequestInterface;
+use Hyperf\Validation\Validator;
+use function Hyperf\Support\make;
 
 abstract class AbstractFormRequestParam extends AbstractParam implements FormRequestParamInterface
 {
     use FormRequestParam;
 
+    /**
+     * 获取已验证的参数
+     *
+     * @author XJ.
+     * @Date   2025/6/15
+     * @return static
+     */
+    public static function validatedAndGet(): static
+    {
+        if (ApplicationContext::getContainer()
+                              ->has(static::class)) {
+            return ApplicationContext::getContainer()
+                                     ->get(static::class);
+        }
+
+        return Context::getOrSet(
+            'validatedGet:' . static::class,
+            function () {
+                $factory = make(Validator::class);
+                $request = make(RequestInterface::class);
+
+                $data          = array_merge_recursive($request->all(), $request->getUploadedFiles());
+                $validatedData = $factory
+                    ->make(
+                        $data,
+                        static::getRules(),
+                        static::getMessages(),
+                        static::getAttributes(),
+                    )
+                    ->validated();
+                $data          = [];
+                foreach (static::getFieldMapping() as $key => $toKey) {
+                    if (isset($validatedData[$key])) {
+                        $data[$toKey] = $validatedData[$key];
+                    }
+                }
+
+                return new static($data);
+            },
+        );
+    }
 }
--- src/Middleware/FormRequestParamValidationMiddleware.php
+++ src/Middleware/FormRequestParamValidationMiddleware.php
@@ -67,11 +67,13 @@ class FormRequestParamValidationMiddleware implements MiddlewareInterface
                             if ($instance->fails()) {
                                 $this->failedValidation($instance);
                             }
-                            $data = $instance->validated();
-                            $data = array_combine(
-                                array_map(fn($v) => $className::getFieldMapping()[$v] ?? $v, array_keys($data)),
-                                array_values($data)
-                            );
+                            $validatedData = $instance->validated();
+                            $data = [];
+                            foreach ($className::getFieldMapping() as $key => $toKey){
+                                if (isset($validatedData[$key])){
+                                    $data[$toKey] = $validatedData[$key];
+                                }
+                            }
                             $this->container->set($className, new $className($data));
                         }
                     }
